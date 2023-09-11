28fdba9acd2ac8de1f7e02e39f1f5c0e8ef3dddf
jaquobia <jaquobia@gmail.com>
2023-09-11 01:19:41 -0400

    Update manual examples to show overflow of u16 at n=27

--- examples/iterate_manual.rs
+++ examples/iterate_manual.rs
@@ -3,7 +3,7 @@ use fibonacci::fibonacci_at_loop;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let term = console::Term::stdout();
-    for n in 0..25 {
+    for n in 0..27 {
         term.read_char()?;
         let fib = fibonacci_at_loop::<u16>(n).ok_or_else(|| "Ran out of memory for integer size!")?;
         println!("{}: {}", n, fib);
--- examples/iterate_manual_no_wait.rs
+++ examples/iterate_manual_no_wait.rs
@@ -2,7 +2,7 @@ use std::error::Error;
 use fibonacci::fibonacci_at_loop;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    for n in 0..25 {
+    for n in 0..27 {
         let fib = fibonacci_at_loop::<u16>(n).ok_or_else(|| "Ran out of memory for integer size!")?;
         println!("{}: {}", n, fib);
     }
